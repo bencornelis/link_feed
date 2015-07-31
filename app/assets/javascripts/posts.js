86470704382af3372633786bc4c61d04e418e83b
Ben Cornelis <bencornelis02@gmail.com>
2015-07-31 16:18:18 -0700

    show more post info when hovering over post listing

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,4 +1,8 @@
 $(function() {
+  $(".post").hover(function() {
+    $(this).find(".post_details .more_info").toggle();
+  });
+
   $(".comment .comment_toggler").click(function() {
     var $toggler = $(this);
     var symbol = $toggler.text();
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
-def sortable(sort_option)
+  def sortable(sort_option)
     is_sorted_by = params[:sort] == sort_option
     link_to sort_option, params.merge({ sort: sort_option }),
                          class: ("btn_blue" if is_sorted_by)
@@ -11,6 +11,10 @@ def sortable(sort_option)
     else
       link_to post.title, post.url
     end
+  end
+
+  def post_type(post)
+    post.text_only? ? "text" : "external link"
   end
 
   def comments_count(commentable)
