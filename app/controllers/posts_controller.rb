e5c7d4feb299bd1dca85a2d0e7dd0a5071f49292
Ben Cornelis <bencornelis02@gmail.com>
2017-07-13 15:47:04 -0500

    refactor global and feed views with partials

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @comments = @post.comments.includes({user: :roles}).arrange(order: "created_at asc")
+    @comments = @post.comments.arrange(order: "created_at asc")
     @comment = Comment.new
   end
 
