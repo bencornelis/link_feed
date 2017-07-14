a39fd973dfc626bae544efbb4f693990dffb0139
Ben Cornelis <bencornelis02@gmail.com>
2017-07-14 18:21:48 -0500

    make javascript work with turbolinks

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,18 +1,5 @@
-$(function() {
+$(document).on('turbolinks:load', function() {
   $(".post").hover(function() {
     $(this).find(".post_details .more_info").toggle();
   });
-
-  $(".comment .comment_toggler").click(function() {
-    var $toggler = $(this);
-    var symbol = $toggler.text();
-    if (symbol == "[+]") {
-      $toggler.text("[-]");
-    } else {
-      $toggler.text("[+]");
-    }
-    $comment = $toggler.closest(".comment");
-    $comment.children(".comment_main").toggle();
-    $comment.next("ul.comments").toggle();
-  });
-})
+});
