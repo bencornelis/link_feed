bbd6830401247ec20c9b7906e9a30ec34fd1f264
Ben Cornelis <bencornelis02@gmail.com>
2017-09-09 14:33:52 -0700

    refactor post specs

--- spec/models/post/global_spec.rb
+++ spec/models/post/global_spec.rb
@@ -2,63 +2,70 @@ require "rails_helper"
 
 describe Post::Global do
   describe "#posts" do
-    subject(:global) { Post::Global.new }
+    subject(:posts) { global.posts }
 
-    it "returns all posts" do
-      posts = create_list(:post, 3)
-      expect(global.posts).to match_array posts
+    context "by default" do
+      let(:global) { Post::Global.new }
+
+      it "orders posts by Hacker News Score" do
+        # score - (S - 1)/(T + 2)^1.8
+        # S - number of shares
+        # T - number of seconds since creation
+
+        all_posts = []
+        Timecop.freeze(1.day.ago) do
+          all_posts << create(:post_with_shares, shares_count: 2)
+          all_posts << create(:post_with_shares, shares_count: 1)
+        end
+
+        expect(posts.to_a).to eql all_posts
+      end
     end
 
-    it "orders posts by Hacker News Score by default" do
-      # score - (S - 1)/(T + 2)^1.8
-      # S - number of shares
-      # T - number of seconds since creation
+    context "with no parameters" do
+      let(:global)     { Post::Global.new }
+      let!(:all_posts) { create_list :post, 3 }
 
-      posts = []
-      Timecop.freeze(1.day.ago) do
-        2.times do |i|
-          posts << create(:post) { |post|
-            post.users_shared_by = create_list(:user, i+1) }
-        end
+      it "returns all posts" do
+        expect(posts).to match_array all_posts
       end
-
-      expect(global.posts).to eq posts.reverse
     end
 
     context "when filtering by tag" do
-      subject(:global) { Post::Global.new(tag: "tag1") }
-      let!(:post1) { create(:post, tag_names: "tag1") }
-      let!(:post2) { create(:post, tag_names: "tag2") }
+      let(:global) { Post::Global.new(tag: "tag1") }
+      let!(:post1) { create :post, tag_names: "tag1"  }
+      let!(:post2) { create :post, tag_names: "tag2"  }
 
       it "only includes posts with that tag" do
-        expect(global.posts).to contain_exactly post1
+        expect(posts).to contain_exactly post1
       end
     end
 
     context "when filtering by page" do
-      subject(:global) { Post::Global.new(page: 2, per_page: 1) }
-      let!(:posts) { create_list(:post, 2) }
+      let(:global) { Post::Global.new(page: 2, per_page: 1) }
+      before { create_list :post, 2 }
 
       it "only shows that page" do
-        expect(global.posts.length).to eq 1
+        expect(posts.length).to eql 1
+        expect(posts.first).to be_a Post
       end
     end
 
     context "when sorting by time" do
-      subject(:global) { Post::Global.new(sort: "time") }
+      let(:global) { Post::Global.new(sort: "time") }
 
       let!(:two_day_old_post) do
-        Timecop.freeze(2.days.ago) { create(:post) }
+        Timecop.freeze(2.days.ago) { create :post }
       end
 
       let!(:day_old_post) do
-        Timecop.freeze(1.day.ago) { create(:post) }
+        Timecop.freeze(1.day.ago) { create :post }
       end
 
-      let!(:new_post) { create(:post, tag_names: "tag1 tag3") }
+      let!(:new_post) { create :post, tag_names: "tag1 tag3" }
 
       it "puts the most recent posts first" do
-        expect(global.posts).to eq [new_post, day_old_post, two_day_old_post]
+        expect(posts.to_a).to eql [new_post, day_old_post, two_day_old_post]
       end
     end
   end
