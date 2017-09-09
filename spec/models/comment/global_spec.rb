a4a042970797f3e775f77a8d8938b049373d7b8c
Ben Cornelis <bencornelis02@gmail.com>
2017-09-09 14:33:41 -0700

    refactor comment specs

--- spec/models/comment/global_spec.rb
+++ spec/models/comment/global_spec.rb
@@ -3,26 +3,31 @@ require "rails_helper"
 describe Comment::Global do
   describe "#comments" do
     let!(:two_day_old_comment) do
-      Timecop.freeze(2.days.ago) { create(:comment) }
+      Timecop.freeze(2.days.ago) { create :comment }
     end
 
     let!(:day_old_comment) do
-      Timecop.freeze(1.day.ago) { create(:comment) }
+      Timecop.freeze(1.day.ago) { create :comment }
     end
 
-    let!(:new_comment) { create(:comment) }
+    let!(:new_comment) { create :comment }
 
-    context "with no parameters" do
-      subject(:global) { Comment::Global.new }
+    subject(:comments) { global.comments }
+
+    context "by default" do
+      let(:global) { Comment::Global.new }
+
       it "orders the comments by date" do
-        expect(global.comments).to eq [new_comment, day_old_comment, two_day_old_comment]
+        expect(comments.to_a)
+          .to eql [new_comment, day_old_comment, two_day_old_comment]
       end
     end
 
     context "when filtering by page" do
-      subject(:global) { Comment::Global.new(page: 3, per_page: 1) }
+      let(:global) { Comment::Global.new(page: 3, per_page: 1) }
+
       it "only shows that page" do
-        expect(global.comments).to eq [two_day_old_comment]
+        expect(comments).to contain_exactly two_day_old_comment
       end
     end
   end
