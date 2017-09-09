fc978b4f01b4d78f79d87aeedfea81f12f009ea8
Ben Cornelis <bencornelis02@gmail.com>
2017-09-09 14:51:20 -0700

    refactor policy specs

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -4,31 +4,29 @@ describe UserPolicy do
   subject { described_class.new(user, other_user) }
 
   context "for a visitor" do
-    let(:user) { nil }
-    let(:other_user) { create(:user) }
+    let(:user)       { nil }
+    let(:other_user) { create :user }
 
     it { should forbid_action(:follow) }
   end
 
   context "for the same user" do
-    let(:user) { create(:user) }
+    let(:user)       { create :user }
     let(:other_user) { user }
 
     it { should forbid_action(:follow) }
   end
 
-  context "for another user already following" do
-    let(:user) { create(:user) }
-    let(:other_user) do
-      create(:user) { |other_user| other_user.followers << user }
-    end
+  context "for another user already followed" do
+    let(:user)       { create :user }
+    let(:other_user) { create :user, followers: [user] }
 
     it { should forbid_action(:follow) }
   end
 
-  context "for another user not following" do
-    let(:user) { create(:user) }
-    let(:other_user) { create(:user) }
+  context "for another user not already followed" do
+    let(:user)       { create :user }
+    let(:other_user) { create :user }
 
     it { should permit_action(:follow) }
   end
