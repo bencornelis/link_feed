4d8b1aa7a5e9e728df686c4c42e51368de431dd0
Ben Cornelis <bencornelis02@gmail.com>
2017-09-09 19:42:54 -0700

    add more post specs and refactor spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,10 +9,41 @@ RSpec.describe Post, type: :model do
   it { should have_many :tags }
 
   it "should assign tags after being saved" do
-    post = build :post, title: "test post", tag_names: "world news"
+    post = build :post, tag_names: "world news"
     post.save!
 
-    tag_names = post.tags.pluck(:name)
-    expect(tag_names).to match_array %w(world news)
+    tags = post.reload.tags
+    expect(tags.pluck(:name)).to match_array %w(world news)
+  end
+
+  describe "#tag_names" do
+    let(:tag_names) { %w(world news) }
+    let(:post) do
+      build :post do |post|
+        tag_names.each { |name| post.tags << build(:tag, name: name) }
+      end
+    end
+
+    it "gets the names of the post's tags" do
+      expect(post.tag_names).to eql tag_names.join(" ")
+    end
+  end
+
+  describe "#text_only?" do
+    context "when the post has a url" do
+      let(:post) { build_stubbed :post, url: "https://www.google.com/" }
+
+      it "is false" do
+        expect(post.text_only?).to be false
+      end
+    end
+
+    context "when the post doesn't have a url" do
+      let(:post) { build_stubbed :post }
+
+      it "is true" do
+        expect(post.text_only?).to be true
+      end
+    end
   end
 end
