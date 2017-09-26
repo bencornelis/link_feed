31e09c6b220d21b0307c9507058913d19b2d56f6
Ben Cornelis <bencornelis02@gmail.com>
2017-09-26 16:35:28 -0700

    load comments for a post using ajax; add loading spinner

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,14 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @comments = @post.comments.arrange(order: "created_at asc")
     @comment = Comment.new
+
+    respond_to do |format|
+      format.html
+      format.js do
+        @comments = @post.comments.arrange(order: "created_at asc")
+      end
+    end
   end
 
   def new
