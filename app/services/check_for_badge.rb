6aac563eae5f0e633f7df8de95defed9b0bd1295
Ben Cornelis <bencornelis02@gmail.com>
2017-10-22 11:02:37 -0700

    fix bug in check_for_badge

--- app/services/check_for_badge.rb
+++ app/services/check_for_badge.rb
@@ -6,10 +6,12 @@ class CheckForBadge
 
   def call
     user.shares_received_since_last_badge += 1
-    return unless user.shares_received_since_last_badge == 10
 
-    user.badges.create
-    user.shares_received_since_last_badge = 0
+    if user.shares_received_since_last_badge == 10
+      user.badges.create
+      user.shares_received_since_last_badge = 0
+    end
+
     user.save
   end
 end
